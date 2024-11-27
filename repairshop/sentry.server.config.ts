bab5ef968b526e015dfc7962c8ef406eaeecd62d
Mateus Colombo Godoy <mateuscgodoy@gmail.com>
2024-11-27 11:51:40 -0300

    feat: add Sentry standard configuration files to the project

---
+++ repairshop/sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://57f4388f4c8d40defad9a5dfb4a6d215@o4508370736447488.ingest.us.sentry.io/4508370744639488",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
